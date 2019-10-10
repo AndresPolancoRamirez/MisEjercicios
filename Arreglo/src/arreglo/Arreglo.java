948dc450c51050d454c07d2775a5d814c5a4927e
AndresPolanco <pipepolanco46@gmail.com>
2019-10-10 16:43:31 -0500

    agregando arreglo

--- Arreglo/src/arreglo/Arreglo.java
+++ Arreglo/src/arreglo/Arreglo.java
@@ -9,15 +9,19 @@ public class Arreglo {
     public static void main(String[] args) {
   
         Scanner input = new Scanner (System.in);
+        double TotalNotas = 0;
         int [] Notas = new int [5];
         
         for (int Estud = 0; Estud <= 4; Estud++){
             System.out.print("Ingrese la calificacion del estuduante N." + (Estud + 1)+ ":"); 
             Notas [Estud]= input.nextInt();
+            TotalNotas = Notas[Estud]; 
         }
+        TotalNotas = TotalNotas / 5;
         for (int Estud = 0; Estud <= 4; Estud++){
          System.out.println("estuduante N." + (Estud + 1)+ ":" + Notas[Estud]);   
         }
+          System.out.println("El promedio de las notas es " + TotalNotas);
         
     }
     
