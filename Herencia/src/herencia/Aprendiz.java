e9bf43610557ef05d349727232d7669d23f03977
AndresPolanco <pipepolanco46@gmail.com>
2019-11-07 09:17:52 -0500

    agregando mis ejercicios

---
+++ Herencia/src/herencia/Aprendiz.java
@@ -0,0 +1,13 @@
+// Autor Andres Polanco
+
+package herencia;
+
+public class Aprendiz extends Persona {
+    private String IdFicha;
+    
+    public Aprendiz (String Nombres, String Apellidos, String NumeroDocumento){
+        
+        
+    }
+    
+}
---
+++ Herencia/src/herencia/Persona.java
@@ -0,0 +1,12 @@
+// Autor Andres Polanco Ramirez
+package herencia;
+
+
+public class Persona {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
