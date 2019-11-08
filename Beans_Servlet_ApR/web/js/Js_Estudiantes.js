b3dbbcd747503008a68bda97756243cf47cd6a8b
AndresPolanco <pipepolanco46@gmail.com>
2019-11-08 14:24:31 -0500

    agregamdo mis ejercicios

---
+++ Beans_Servlet_ApR/web/js/Js_Estudiantes.js
@@ -0,0 +1,40 @@
+function Registrar_Estudiantes(){
+    
+    var Nombre = $("#Nombre").val();
+    var Nota1 = $("#Nota1").val();
+    var Nota2 = $("#Nota2").val();
+    var Nota3 = $("#Nota3").val();
+ 
+    var parametros = { 'Operacion' : 'Registrar','N1':Nota1,'N2':Nota2,'N3':Nota3,'Nomb':Nombre};
+  
+  $.ajax({
+      type: "POST",
+      url: "Cont_Estudiantes",
+      data: parametros,
+      dataType: "text",
+      success: function (resp){
+          $("#Nombre").val('');
+          $("#Nota1").val('');
+          $("#Nota2").val('');
+          $("#Nota3").val('');
+          $('#respuesta_servidor').html(resp);
+      }
+  }).fail (function (jqXHR, textStatus, errorThrown){
+      alert("Error : " + jqXHR.status);
+  });  
+}
+
+function Listar_Estudiantes(){
+    var parametros = { 'Operacion':'Listar'};
+    $.ajax({
+        type: "POST",
+        url: "Cont_Estudiantes",
+        data: parametros,
+        dataType: "text",
+        success: function (resp){
+            $('#Listar_Estudiantes').html(resp);
+        }
+    }).fail (function (jqXHR, textStatus, errorThrown){
+      alert("Error : " + jqXHR.status);
+  });
+}
--- HolaMundoFK/src/holamundofk/HolaMundoFK.java
+++ HolaMundoFK/src/holamundofk/HolaMundoFK.java
@@ -8,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import javafx.scene.control.Label;
 
 /**
